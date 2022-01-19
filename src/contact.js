44273925fab4ad3cd2b10416c676cfc1739ce2f4
Gravity459 <59308294+Gravity459@users.noreply.github.com>
2022-01-19 23:24:38 +0500

    contact in js implemented fully

--- src/contact.js
+++ src/contact.js
@@ -1,46 +1,59 @@
-function createDivImage(url) {
-  const newDiv = document.createElement("div");
+function createImageDiv(url) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add('image-area');
 
-  const image = document.createElement("img");
-  image.setAttribute("src", url);
+    const image = document.createElement("img");
+    image.setAttribute("src", url);
 
-  newDiv.appendChild(image);
-
-  return newDiv;
+    newDiv.appendChild(image);
+    return newDiv;
 }
+
 
 function createLinkDiv(link) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("desc-area");
 
-    const newLinkTag = document.createElement("a"); 
-    newLinkTag.setAttribute("href", link);
+    const linkTag = document.createElement("a"); 
+    linkTag.setAttribute("href", link);
 
-}
-
-function createH1() {
-
+    linkTag.appendChild(createH1('Gravity459'));
     
+    newDiv.appendChild(linkTag);
+    newDiv.appendChild(createP("The best of the best burgerista in the universe."));
+    newDiv.appendChild(createP("Contact: 1234-GRAV(4728)"));
 
+    return newDiv;
 }
 
+function createP(text) {
+    const ptag = document.createElement("p");
+    ptag.textContent += text;
 
-function createDiv(classname) {
-  const newDiv = document.createElement("div");
-  
+    return ptag;
+}
 
+function createH1(text) {
+    const h1tag = document.createElement("h1");
+    h1tag.textContent = text;
 
-  return newDiv;
+    return h1tag;
+}
+
+function createContact(contact) {
+    contact.appendChild(createImageDiv("../images/profile.jpg"));
+    contact.appendChild(createLinkDiv("https://github.com/Gravity459"));
 }
 
 function contactModule() {
 
-  var contactarea = document.createElement("div");
-  contactarea.classList.add("main-area");
-  contactarea.classList.add("contact-area");
+    var contactarea = document.createElement("div");
+    contactarea.classList.add("main-area");
+    contactarea.classList.add("contact-area");
 
+    createContact(contactarea);
     
-
-
-  return contactarea;
+    return contactarea;
 }
 
 export default contactModule;
